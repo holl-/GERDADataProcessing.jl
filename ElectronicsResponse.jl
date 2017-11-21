a46a4cbcfa9063ea0ba164ad6f3f079eb8fd817f
Anna Julia Zsigmond <zsigi3@gmail.com>
2017-11-21 15:36:00 +0100

    upload first files

---
+++ ElectronicsResponse.jl
@@ -0,0 +1,46 @@
+using DSP
+
+function applyElectronics(pulse; Ts = 10e-9, GBP = 2750e6, tau = 180e-6, Kv = 150e3, Cd = 50e-12, Cf = 0.35e-12, Rf = 500e6)
+
+    wop = GBP / (2 * pi * Kv)
+    Cmod = Cf + Cd
+    wmod = 1.0 / (Rf * Cmod)
+    alfa = Cmod / (Cf * GBP)
+
+    b0 = 1.0 / alfa
+    a2 = 1.0
+    a1 = 1.0 / alfa + wop + wmod
+    a0 = 1.0 / (tau * alfa) + wmod*wop
+
+    # then the transfer function in the *Laplace* s-domain looks like this:
+    #                       b0
+    #   T(s) = ----------------------------
+    #             a2 * s^2 + a1 * s + a0
+
+    # PolynomialRatio needs z-transform paramters: s- and z-domains can be connected by
+    # the bilinear transform:
+    #        z - 1
+    # s = K -------- , K = Ts/2  , Ts - sampling period
+    #        z + 1 
+    #        
+    # we can then convert T(s) to T(z):
+    #              bz2 * z^2 + bz1 * z + bz0
+    #   T(z) = -------------------------------
+    #              az2 * z^2 + az1 * z + az0
+    #
+
+    K = 2/Ts
+
+    az2 = 1.0   # normalized
+    az1 = (2*a0 - 2*K^2)/(K^2 + a1*K + a0)
+    az0 = (K^2 - a1*K + a0)/(K^2 + a1*K + a0)
+
+    bz2 = b0/(K^2 + a1*K + a0)
+    bz1 = 2*b0/(K^2 + a1*K + a0)
+    bz0 = b0/(K^2 + a1*K + a0)
+
+    myfilter = PolynomialRatio([bz2, bz1, bz0], [az2, az1, az0])
+
+    filtered = filt(myfilter, vcat([0], diff(pulse)))
+
+end
---
+++ WaveformFunctions.jl
@@ -0,0 +1,77 @@
+function find_intersect(samples::Vector, threshold::Real, noisefilter = 1)
+
+    if noisefilter < 1 noisefilter = 1 end
+    if length(samples) < 1 error("Error: empty array") end
+    i = 1
+    x = samples[i]
+    counter = 0
+    intersect = i
+    if x != threshold
+        findHigher = x < threshold
+        while i < length(samples)
+            i += 1
+            x = samples[i]
+            if (findHigher && (x >= threshold)) || (!findHigher && (x <= threshold))
+                if counter == 0
+                    intersect = i
+                end
+                if counter >= noisefilter-1
+                    return intersect
+                else
+                    counter += 1
+                end
+            else
+                counter = 0
+            end
+        end
+        println("No intersect found")
+        return NaN
+    end
+    return NaN
+
+end
+
+
+function get_risetime(samples::Vector, lowfraction = 0.05, highfraction = 0.95, noisefilter = 1, samplingtime = 1)
+
+    lowindex = find_intersect(samples, lowfraction * maximum(samples), noisefilter)
+    highindex = find_intersect(samples, highfraction * maximum(samples), noisefilter)
+    risetime = (highindex - lowindex) * samplingtime
+
+end
+
+
+function resample_wf(samples::Vector, factor::Integer)
+    if rem(length(samples), factor) != 0
+        error("Error: resampling does not work with your factor")
+    end
+    n_out = convert(Int64, length(samples)/factor)
+    wf_out = zeros(typeof(samples[1]), n_out)
+    for j in 1:n_out
+        wf_out[j] = sum(samples[((j-1)*factor+1):(j*factor)])/factor
+    end
+    wf_out
+end
+
+
+function deconv_tau(samples::Vector, tau::Real)
+
+    alpha = 1 - exp(- 1/tau)
+    acc = zero(eltype(samples))
+    newsamples = zeros(samples)
+    for i in eachindex(samples)
+        x = samples[i]
+        new_x = x + acc
+        acc += x * alpha
+        newsamples[i] = new_x
+    end
+    newsamples
+
+end
+
+
+function deconv_tau!(samples::Vector, tau::Real)
+
+    samples = deconv_tau(samples, tau)
+
+end
